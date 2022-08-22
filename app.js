8417917c98e50496c5ff66ec463905e86d7eac9a
suleman893 <sulemanahmad893@gmail.com>
2022-08-22 11:57:30 +0500

    Resolved issue of payload too large

--- app.js
+++ app.js
@@ -16,13 +16,19 @@ app.use(cors());
 
 app.use(express.static("public"));
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 
 app.use(cookieParser());
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => app.listen(process.env.DB_PORT))
-  .then(() => console.log(`Server started running on port ${process.env.DB_PORT}`))
+  .then(() =>
+    console.log(`Server started running on port ${process.env.DB_PORT}`)
+  )
   .catch((err) => console.log(err));
 
 app.get("*", checkUser);
