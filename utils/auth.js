904d98679a86786cdf5f35f9c847e22080750e6d
Adnan <malikadnan655@gmail.com>
2022-09-16 15:48:56 +0500

    ✨ auth fix

--- utils/auth.js
+++ utils/auth.js
@@ -1,23 +1,21 @@
 const jwt = require("jsonwebtoken");
+const { StatusCodes } = require("http-status-codes");
 const User = require("../models/user");
 const catchAsync = require("../utils/catchAsync");
 require("dotenv").config();
 
 const requireAuth = catchAsync(async (req, res, next) => {
     const token = req.headers.jwt;
-    
     if (token) {
         jwt.verify(token, process.env.JWT_KEY, (err, decodedToken) => {
             if (err) {
-                res.status(400).json("Your token has expired, kindly login again.");
+                res.status(400).json("Your token has expired, kindly login again!");
             } else {
                 next();
             }
         });
     } else {
-        setTimeout(() => {
-            res.redirect("/login");
-        }, 1000);
+        res.status(StatusCodes.BAD_REQUEST).json('You are not logged in. Kindly login again!')
     }
 })
 
