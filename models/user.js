79a57c3f09c0d7c516aa477eab9252bf30992f1d
muhammadadnan655 <muhammadadnan.webevis@gmail.com>
2022-09-20 17:14:19 +0500

    🚀 admin profile schema and api

--- models/user.js
+++ models/user.js
@@ -66,6 +66,7 @@ const UserSchema = new mongoose.Schema(
 UserSchema.pre("save", async function (next) {
   const salt = await bycrypt.genSalt(10);
   this.password = await bycrypt.hash(this.password, salt);
+  this.confirmPassword = await bycrypt.hash(this.confirmPassword, salt);
   next();
 });
 UserSchema.methods.checkPassword = async function (password) {
