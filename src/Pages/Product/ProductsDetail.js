500d2d17b327405492031b8937f94ebfe29b544f
suleman-ahmad-webevis <suleman@webevis.com>
2023-05-05 16:17:32 +0500

    Sidebar and product detail image fix

--- src/Pages/Product/ProductsDetail.js
+++ src/Pages/Product/ProductsDetail.js
@@ -26,9 +26,9 @@ const ProductsDetail = ({ showSidebar, setShowSidebar }) => {
 
 const ProductsListContainer = styled.div`
   width: 100%;
-  margin-top: 100px;
+  margin-top: 30px;
   @media screen and (max-width: 1110px) {
-    margin-top: 50px;
+    margin-top: 30px;
   }
 `;
 export default ProductsDetail;
