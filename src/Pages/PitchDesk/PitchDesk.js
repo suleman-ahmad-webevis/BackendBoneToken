57059bcb74b107107483ce04e6c2cce406a6721f
suleman-ahmad-webevis <suleman@webevis.com>
2023-05-10 22:00:31 +0500

    Fixed pdf

--- src/Pages/PitchDesk/PitchDesk.js
+++ src/Pages/PitchDesk/PitchDesk.js
@@ -1,25 +1,13 @@
-import React, { useState } from "react";
-//ToShowPDF
-import { Viewer, Worker } from "@react-pdf-viewer/core";
-import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
-import "@react-pdf-viewer/core/lib/styles/index.css";
-import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 import PirchDeck from "../../assets/pdf/Faq/Pirchdeck.pdf";
-import { PDFContainer } from "../WhitePaper/WhitePaper";
 
 const PPComp = () => {
-  const [defaultPdfFile] = useState(PirchDeck);
-  const defLayoutPlugin = defaultLayoutPlugin();
   return (
-    <PDFContainer>
-      {defaultPdfFile && (
-        <>
-          <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.min.js">
-            <Viewer fileUrl={defaultPdfFile} plugins={[defLayoutPlugin]} />
-          </Worker>
-        </>
-      )}
-    </PDFContainer>
+    <object
+      width="100%"
+      height="850px"
+      data={PirchDeck}
+      type="application/pdf"
+    ></object>
   );
 };
 
--- src/Pages/WhitePaper/WhitePaper.js
+++ src/Pages/WhitePaper/WhitePaper.js
@@ -1,33 +1,14 @@
-import React, { useState } from "react";
-//ToShowPDF
-import { Viewer, Worker } from "@react-pdf-viewer/core";
-import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
-import "@react-pdf-viewer/core/lib/styles/index.css";
-import "@react-pdf-viewer/default-layout/lib/styles/index.css";
-import styled from "styled-components";
 import WhitePaper from "../../assets/pdf/Faq/Whitepaper.pdf";
 
 const WPComp = () => {
-  const [defaultPdfFile] = useState(WhitePaper);
-  const defLayoutPlugin = defaultLayoutPlugin();
-
   return (
-    <PDFContainer>
-      {defaultPdfFile && (
-        <>
-          <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.min.js">
-            <Viewer fileUrl={defaultPdfFile} plugins={[defLayoutPlugin]} />
-          </Worker>
-        </>
-      )}
-    </PDFContainer>
+    <object
+      width="100%"
+      height="850px"
+      data={WhitePaper}
+      type="application/pdf"
+    ></object>
   );
 };
 
 export default WPComp;
-
-export const PDFContainer = styled.div`
-  height: 1780px;
-  max-width: 1180px;
-  margin: 0 auto;
-`;
