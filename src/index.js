8e6d56339e504aae5c87492a7021dfd0ab98a6ef
suleman-ahmad-webevis <suleman@webevis.com>
2023-05-12 12:27:02 +0500

    Commented fb sdk

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
-import initFacebookSDK from "./utils/InitFacebookSDK";
+// import initFacebookSDK from "./utils/InitFacebookSDK";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 //MainRouteFile
 import DASHBOARD from "./routes/Dashboard";
@@ -197,6 +197,6 @@ const renderApp = () => {
   );
 };
 
-initFacebookSDK().then(() => {
-  renderApp();
-});
+// initFacebookSDK().then(() => {
+//   renderApp();
+// });
