a6b84c91a65a9d34cc6f605d1f88b711e7f5f938
suleman893 <sulemanahmad893@gmail.com>
2023-06-19 19:00:41 +0500

    Origin

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const supplierRoutes = require("./routes/supplierRoutes");
 const paymentRoutes = require("./routes/paymentRoutes");
 const { StatusCodes } = require("http-status-codes");
 //Middleware
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json({ limit: "500mb" }));
 app.use(express.urlencoded({ extended: true, limit: "500mb" }));
 
